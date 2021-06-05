5ce066a1c4747b147308c8314c5cc1337f9e37c0
seipan1213 <seiju1213@gmail.com>
2021-06-05 17:58:23 +0900

    タスク管理状態保持

--- myapp/app/Models/Folder.php
+++ myapp/app/Models/Folder.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Folder extends Model
 {
-    use HasFactory;
+	public function tasks()
+	{
+		return $this->hasMany(Task::class);
+	}
 }
