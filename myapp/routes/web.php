2231b9cab6c7cd2f2775013eaab0aebe46171545
seipan1213 <seiju1213@gmail.com>
2021-06-05 13:14:55 +0900

    フォルダ作成ページ 仮実装

--- myapp/routes/web.php
+++ myapp/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/folders/{id}/tasks', [TaskController::class, 'index'])->name('tasks.index');
